1df99494ec3eeec18306d0484e5849e23ec7011a
CC <50982416+cyndereN@users.noreply.github.com>
2020-07-31 23:29:03 +0100

    Create pong 3

--- Code/Lua/pong/pong 1/main.lua
+++ Code/Lua/pong/pong 1/main.lua
@@ -10,9 +10,6 @@ function love.load()
 	
 	love.graphics.setDefaultFilter('nearest','nearest')
 
-	smallFont = love.graphics.newFont('font.ttf')
-	love.graphics.setFont(smallFont)
-
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
 		fullscreen = false,
 		vsync = true,
@@ -28,20 +25,12 @@ function love.keypresssed(key)
 end
 
 function love.draw()
-
 	push:apply('start')
-	
-	love.graphics.clear(40/255, 45/255, 52/255, 255/255)
-
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
-
-	love.graphics.rectangle('fill', 5, 20, 5, 20)
-	love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
 
 	love.graphics.printf(
 		"Hello Pong!", 
 		0, 
-		20, 
+		VIRTUAL_HEIGHT / 2 - 6, 
 		VIRTUAL_WIDTH, 
 		'center')
 
