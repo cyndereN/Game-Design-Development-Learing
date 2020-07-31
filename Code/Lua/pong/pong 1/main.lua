763330ee5c5335d903b480f38c8a02e0c66e52aa
CC <50982416+cyndereN@users.noreply.github.com>
2020-07-31 23:27:29 +0100

    Update main.lua

--- Code/Lua/pong/pong 1/main.lua
+++ Code/Lua/pong/pong 1/main.lua
@@ -28,7 +28,15 @@ function love.keypresssed(key)
 end
 
 function love.draw()
+
 	push:apply('start')
+	
+	love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+
+	love.graphics.rectangle('fill', 5, 20, 5, 20)
+	love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
 
 	love.graphics.printf(
 		"Hello Pong!", 
