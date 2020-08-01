8f3d8adb9af0cd42ee35a9e11513e018d5fc55c9
CC <50982416+cyndereN@users.noreply.github.com>
2020-08-01 17:55:37 +0100

    Update main.lua

--- Code/Lua/pong/pong 4/main.lua
+++ Code/Lua/pong/pong 4/main.lua
@@ -22,6 +22,14 @@ function love.load()
 	player1Y = 30
 	player2Y = VIRTUAL_HEIGHT - 50
 
+	ballX = VIRTUAL_WIDTH / 2 - 2
+	ballY = VIRTUAL_HEIGHT / 2 - 2
+
+	ballDX = math.random(2) == 1 and -100 or 100
+	ballDY = math.random(-50, 50)
+
+	gameState = 'start'
+
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
 		fullscreen = false,
 		vsync = true,
@@ -42,11 +50,28 @@ function love.update(dt)
 		player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
 	end
 
+	if gameState == 'play'then
+		ballX = ballX + ballDX * dt
+		ballY = ballY + ballDY * dt
+	end
 end
+
 
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.quit()
+	elseif key == 'enter' or key == 'return' then
+		if gameState == 'start' then
+			gameState = 'play' 
+		elseif gameState == 'play' then
+			gameState = 'start'
+
+			ballX = VIRTUAL_WIDTH / 2 - 2
+			ballY = VIRTUAL_HEIGHT / 2 - 2
+
+			ballDX = math.random(2) == 1 and -100 or 100
+			ballDY = math.random(-50, 50)
+		end
 	end
 end
 
@@ -57,13 +82,19 @@ function love.draw()
 	love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
 	love.graphics.setFont(smallFont)
-	love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+	if gameState == 'start'
+		love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+	end
+
+	if gameState == 'play'
+		love.graphics.printf("Play State!", 0, 20, VIRTUAL_WIDTH, 'center')
+	end
 
 	love.graphics.setFont(scoreFont)
 	love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
 	love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+	love.graphics.rectangle('fill', ballX, ballY, 4, 4)
 
 	love.graphics.rectangle('fill', 20, player1Y, 5, 20)
 	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 25, player2Y, 5, 20)
