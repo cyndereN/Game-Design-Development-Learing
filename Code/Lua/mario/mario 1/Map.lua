4d55c7a0042e81f7b8ba43fce1461fa985b7dbbd
CC <50982416+cyndereN@users.noreply.github.com>
2020-09-06 17:47:08 +0100

    Update mario 1 and create mario 2

--- Code/Lua/mario/mario 1/Map.lua
+++ Code/Lua/mario/mario 1/Map.lua
@@ -1,7 +1,10 @@
+require 'Util'
 Map = Class{}
 
 TILE_BRICK = 1
-TILE_EMPTY = -1
+TILE_EMPTY = 4
+
+local SCROLL_SPEED = 62
 
 function Map:init()
     self.spritesheet = love.graphics.newImage('graphics/spritesheet.png')
@@ -11,11 +14,20 @@ function Map:init()
     self.mapHeight = 28
     self.tiles = {}
 
+    self.camX = 0
+    self.camY = 0
+
     self.tileSprites = generateQuads(self.spritesheet, self.tileWidth, self.tileHeight)
 
     for y = 1, self.mapHeight do
         for x = 1, self.mapWidth do
             self:setTile(x, y, TILE_EMPTY)
+        end
+    end
+
+    for y = self.mapHeight / 2, self.mapHeight do
+        for x = 1, self.mapWidth do
+            self:setTile(x, y, TILE_BRICK)
         end
     end
 end
@@ -29,9 +41,14 @@ function Map:getTile(x, y)
 end
 
 function Map:update(dt)
-
+    self.camX = self.camX + SCROLL_SPEED * dt
 end
 
 function Map:render()
-
+    for y = 1, self.mapHeight do
+        for x = 1, self.mapWidth do
+            love.graphics.draw(self.spritesheet, self.tileSprites[self:getTile(x, y)],
+                (x - 1) * self.tileWidth, (y - 1) * self.tileHeight)
+        end
+    end
 end
